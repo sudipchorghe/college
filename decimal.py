f59a6b9ebd13b77084d3b108bb7f8f0c8347954c
sudip <sudipchorghe@gmail.com>
2025-12-28 18:50:22 +0530

    decimal to binary octal and hex converter

---
+++ decimal.py
@@ -0,0 +1,16 @@
+decimal = int(input("enter a decimal value:  "))
+print()
+
+binary = bin(decimal)
+octal = oct(decimal)
+
+hexadecimal = hex(decimal)
+
+print("------the values of " , decimal ,"in-------" )
+print("decimal      :" , decimal)
+print("binary       :" , binary)
+print("octal        :" , octal)
+print("hexadecimal  :" , hexadecimal)
+
+
+
