892ef7aa5c8a896b32a0c7f7b62dd2a7facc34c0
sudip <sudipchorghe@gmail.com>
2026-01-02 18:38:43 +0530

    added choice selector to the code

--- pratical2 .py
+++ pratical2 .py
@@ -1,72 +1,79 @@
-# f = float(input("enter the frequency in hz "))
-# if f <= 20000:
-#     print ("audio frequency is (am)")
-                           
-# else:
-#     print(" audio frequency is (rf)")
+print("Enter a choice of code")
+choice = int(input("Enter your choice (1-5): "))
 
+match choice:
+    case 1:
+        # Audio frequency check
+        f = float(input("Enter the frequency in Hz: "))
+        if f <= 20000:
+            print("Audio frequency is (AM)")
+        else:
+            print("Audio frequency is (RF)")
 
-#code 2 
-# f = float(input("enter frequency in hz :"))
-# if 30000 <= f <= 300000:
-#     print(" VLF band ")
-# elif 300000 <= f <= 3000000:
-#     print("LF band")
-# elif 3000000 <= f <= 30000000:
-#     print("HF band")
-# elif 30000000 <= f <= 300000000:
-#     print("VHF band")
-# else:
-#     print("frequency not in standard freauency range")
-    
-#code 3
-# resistor value by color
-# color = input("enter the color of band : ")
+    case 2:
+        # Frequency band classification
+        f = float(input("Enter frequency in Hz: "))
+        if 30000 <= f <= 300000:
+            print("VLF band")
+        elif 300000 <= f <= 3000000:
+            print("LF band")
+        elif 3000000 <= f <= 30000000:
+            print("HF band")
+        elif 30000000 <= f <= 300000000:
+            print("VHF band")
+        else:
+            print("Frequency not in standard range")
 
-# if color == "black":
-#     print("the value of band is = zero (0)")
-# elif color == "brown":
-#     print("the value of band is = one (1)")
-# elif color == "red":
-#     print("the value of band is = two (2)")
-# elif color == "orange":
-#     print("the value of band is = three (3)")
-# elif color == "yellow":
-#     print("the value of band is = four (4)")
-# elif color == "green":
-#     print("the value of band is = five (5)")
-# elif color == "blue":
-#     print("the value of band is = six (6)")
-# elif color == "voilet":
-#     print("the value of band is = seven (7)")
-# elif color == "grey":
-#     print("the value of band is = eight (8)")
-# elif color == "white":
-#     print("the value of band is = nine (9)")
-# else:
-#     print("enter a valid color")
-#code 4 
-# voltage = float(input("enter the voltage of battery")) 
-# if 1 <= voltage <= 3.7:
-# print("battery is charging") 
-# elif 3.7 <= voltage <= 4.2: print("battery is charged") 
-# else: print("over/under voltage, battery is damaged!!!")
-    
-#code 5
+    case 3:
+        # Resistor value by color
+        color = input("Enter the color of band: ").lower()
+        if color == "black":
+            print("The value of band is = 0")
+        elif color == "brown":
+            print("The value of band is = 1")
+        elif color == "red":
+            print("The value of band is = 2")
+        elif color == "orange":
+            print("The value of band is = 3")
+        elif color == "yellow":
+            print("The value of band is = 4")
+        elif color == "green":
+            print("The value of band is = 5")
+        elif color == "blue":
+            print("The value of band is = 6")
+        elif color == "violet":
+            print("The value of band is = 7")
+        elif color == "grey":
+            print("The value of band is = 8")
+        elif color == "white":
+            print("The value of band is = 9")
+        else:
+            print("Enter a valid color")
 
-# #tolerence checker
-# nominal_resistance = 100
-# tolerence_percent = 5
-# #measured resistance
-# measured_resistance = 103
+    case 4:
+        # Battery voltage checker
+        voltage = float(input("Enter the voltage of battery: "))
+        if 1 <= voltage <= 3.7:
+            print("Battery is charging")
+        elif 3.7 <= voltage <= 4.2:
+            print("Battery is charged")
+        else:
+            print("Over/under voltage, battery is damaged!!!")
 
-# tolerance = nominal_resistance* (tolerence_percent / 100)
-# lower_limit = nominal_resistance - tolerance
-# upper_limit = nominal_resistance + tolerance
+    case 5:
+        # Tolerance checker
+        nominal_resistance = 100
+        tolerance_percent = 5
+        measured_resistance = float(input("Enter measured resistance: "))
 
-# #check
+        tolerance = nominal_resistance * (tolerance_percent / 100)
+        lower_limit = nominal_resistance - tolerance
+        upper_limit = nominal_resistance + tolerance
 
-# if lower_limit <= measured_resistance <= upper_limit:
-#     print("the resister is within tolrence")
-# else:
-#     print("the resistor is outside tolernece range")
+        if lower_limit <= measured_resistance <= upper_limit:
+            print("The resistor is within tolerance")
+        else:
+            print("The resistor is outside tolerance range")
+
+    case _:
+        print("Invalid choice")
